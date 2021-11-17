a5a02370ad2ac6f085cffa68ba27bb2e6e7d5b6a
huwenfan <huhuang03@gmail.com>
2021-11-17 15:06:56 +0800

    ilt's look like that the babel module-resolver works but metro still can't find the library out of current source

--- example/babel.config.js
+++ example/babel.config.js
@@ -3,14 +3,13 @@ const path = require('path');
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    // [
-    //   'module-resolver',
-    //   {
-    //     root: ['.'],
-    //     alias: {
-    //       // ['th_views.rn']: path.join(__dirname, '..', 'src', 'index.ts'),
-    //     },
-    //   },
-    // ],
+    [
+      'module-resolver',
+      {
+        alias: {
+          'th_views.rn': path.join(__dirname, '..', 'src', 'index.ts'),
+        },
+      },
+    ],
   ],
 };
