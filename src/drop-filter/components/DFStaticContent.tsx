838ed3a231e24b0051aa69208f803aa38837565b
huwenfan <huhuang03@gmail.com>
2021-12-02 17:37:07 +0800

    up

--- src/drop-filter/components/DFStaticContent.tsx
+++ src/drop-filter/components/DFStaticContent.tsx
@@ -1,44 +1,44 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
-import {DFDataItem, DFSelectItem} from '../model';
-import DFListItem from './DFListItem';
-
-export interface DFStaticContentProps {
-  // so what this data present?
-  data: DFDataItem;
-}
-
-// any better idea to present the select?
-const DFStaticContent: React.FC<DFStaticContentProps> = props => {
-  const {data} = props;
-  // const [select, setSelect] = useState<DFSelectItem | undefined>(undefined);
-
-  // let curSelect: DFSelectItem | undefined = {
-  //   level: -1,
-  //   child: select,
-  // }
-
-  return (
-    <View
-      style={{
-        flexDirection: 'row',
-      }}>
-
-      {data.list?.map((item, index) => {
-        // curSelect = curSelect?.child;
-        // ok, please do some thing.
-        const rst = <DFListItem
-          key={`${index}`}
-          // title need get from parent select.
-          title={''} onClick={() => {}} addAll={}/>;
-        return rst;
-      })}
-
-    </View>
-    // <View>
-    //   <Text>DFStaticContent</Text>
-    // </View>
-  );
-};
-
-export default DFStaticContent;
+// import React, {useState} from 'react';
+// import {View, Text} from 'react-native';
+// import {DFDataItem, DFSelectItem} from '../model';
+// import DFListItem from './DFListItem';
+//
+// export interface DFStaticContentProps {
+//   // so what this data present?
+//   data: DFDataItem;
+// }
+//
+// // any better idea to present the select?
+// const DFStaticContent: React.FC<DFStaticContentProps> = props => {
+//   const {data} = props;
+//   // const [select, setSelect] = useState<DFSelectItem | undefined>(undefined);
+//
+//   // let curSelect: DFSelectItem | undefined = {
+//   //   level: -1,
+//   //   child: select,
+//   // }
+//
+//   return (
+//     <View
+//       style={{
+//         flexDirection: 'row',
+//       }}>
+//
+//       {data.list?.map((item, index) => {
+//         // curSelect = curSelect?.child;
+//         // ok, please do some thing.
+//         const rst = <DFListItem
+//           key={`${index}`}
+//           // title need get from parent select.
+//           title={''} onClick={() => {}} addAll={}/>;
+//         return rst;
+//       })}
+//
+//     </View>
+//     // <View>
+//     //   <Text>DFStaticContent</Text>
+//     // </View>
+//   );
+// };
+//
+// export default DFStaticContent;
